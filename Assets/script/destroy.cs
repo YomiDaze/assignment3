a989de8eeecadfdf3629de3f3cc0b3a7d800d725
YomiDaze <61341370+YomiDaze@users.noreply.github.com>
2020-04-04 00:29:14 +0700

    homework3

---
+++ Assets/script/destroy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    public GameObject startPointStage2;
+
+    void Start()
+    {
+        transform.position = new Vector3(-10.01f, 1.1565f, 9.58f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public GameObject goalgameObject;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("WIN");
+    }
+    void OnCollisionEnter(Collision Des)
+    {
+        if (Des.gameObject.name == "player")
+        {
+            Destroy(GameObject.Find("target"));
+        }
+        if (Des.gameObject.name =="player")
+        {
+            Destroy(GameObject.Find("target2"));
+        }
+    }
+    
+}
---
+++ Assets/script/dragObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosWorld = new Vector3(Input.mousePosition.x,Input.mousePosition.y,5f);
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosWorld);
+        transform.position = objectPosition;
+    }
+    
+}
